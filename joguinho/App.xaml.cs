9a3567d89b092658dc364e1d1c66533c330c0f6d
Eduardo <dukahenrick06@gmail.com>
2024-04-04 09:23:59 -0300

    Projeto Limpo

---
+++ joguinho/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace joguinho;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ joguinho/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace joguinho;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
